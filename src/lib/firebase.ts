0fb1979e77d98048cd8e1a2dc4d178eadb2431bf
ddaa81108-wq <ddaa81108@gmail.com>
2026-07-19 15:04:36 +0200

    fix: embed Firebase config directly in lib/firebase.ts, remove json import

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import config from "../../firebase-applet-config.json";
 
 const firebaseConfig = {
-  apiKey: config.apiKey,
-  authDomain: config.authDomain,
-  projectId: config.projectId,
-  storageBucket: config.storageBucket,
-  messagingSenderId: config.messagingSenderId,
-  appId: config.appId,
-  measurementId: (config as any).measurementId,
+  apiKey: "AIzaSyBy-AaKnxHSxFS4Gud_QaVEw6AZGFC-YGg",
+  authDomain: "abdocash121.firebaseapp.com",
+  projectId: "abdocash121",
+  storageBucket: "abdocash121.firebasestorage.app",
+  messagingSenderId: "24328376389",
+  appId: "1:24328376389:web:252876b9522cfdbed2932c"
 };
 
 const app = initializeApp(firebaseConfig);
